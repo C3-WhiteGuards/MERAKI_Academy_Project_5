41cb293a8e3fc948ae877df9ef3ac48397040f44
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-10-07 22:02:26 +0300

    gym router finished

---
+++ backend/routers/routes/gym.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const {createNewGym} = require("./../controllers/gym")
+const gymRouter = express.Router();
+gymRouter.post("/",createNewGym)
+module.exports = gymRouter
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ app.use(express.json());
 app.use(cors());
 
 // ruqia 
-
+const gymRouter = require("./routers/routes/gym")
+app.use("/gym" , gymRouter)
 
 
 
