8a5dc496dd5929c4a7a9d8f3d9eb8a3a46a18560
G11_Kulthoom <kulthumradi94@gmail.com>
2021-10-08 21:10:00 +0300

    creating the get all resturent function and adding its route to the resturant router

--- backend/routers/controllers/resturant.js
+++ backend/routers/controllers/resturant.js
@@ -33,6 +33,21 @@ const query = `INSERT INTO resturant (
             });
 
 }
+const getAllResturants = (req,res)=>{
+  const query = `SELECT * FROM articles WHERE is_deleted=0 `
+  connection.query(query , (error, result)=>{
+
+    if (error) {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        error: error,
+      });
+    }
+    res.status(200).json({success : true , message:" all resturants ", result:result});
+  });
+  };
+  
 
 
-module.exports = {createNewResturent}
+module.exports = {createNewResturent , getAllResturants }
