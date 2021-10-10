f21c2a0e1e222f4b939eb27aaf361939fbfc4dbd
G11_Kulthoom <kulthumradi94@gmail.com>
2021-10-10 23:29:27 +0300

    creating the router and the route for the delete  by id

--- backend/routers/routes/comment.js
+++ backend/routers/routes/comment.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { createNewComment ,updateResturantById ,deleteResturantById } = require("../controllers/comment");
+const { createNewComment ,updateResturantById ,deleteCommentById } = require("../controllers/comment");
 
 const commentRouter = express.Router();
 
 commentRouter.post("/", createNewComment);
 commentRouter.put("/:id", updateResturantById);
+
+commentRouter.delete("/:id", deleteCommentById);
 
 module.exports = commentRouter;
