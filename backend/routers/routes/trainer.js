f7a40c7cd061780ca46a5784b2cbb67e66630d04
AbdullahNaser94 <abdulahnaser94@gmail.com>
2021-10-10 22:28:13 +0300

    add router in file trainer.js

--- backend/routers/routes/trainer.js
+++ backend/routers/routes/trainer.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { createNewTrainer , updateTrainerById , getTrainerById } = require("../controllers/trainer");
- 
+const {
+  createNewTrainer,
+  updateTrainerById,
+  getTrainerById,
+  getAllTrainer,
+} = require("../controllers/trainer");
+
 const trainerRouter = express.Router();
 trainerRouter.post("/", createNewTrainer);
 trainerRouter.put("/:id", updateTrainerById);
 trainerRouter.get("/:id", getTrainerById);
+trainerRouter.get("/", getAllTrainer);
 
 module.exports = trainerRouter;
