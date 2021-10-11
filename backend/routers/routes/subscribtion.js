087158d79cffb0ce40fd87fe60ef0d203a1372af
RashedMigdady <migdadyrashed@gmail.com>
2021-10-11 18:15:49 +0300

    create add new user subscription with trainer

---
+++ backend/routers/routes/subscribtion.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { addGymUser, addRestaruntUser, addTrainerUser } = require('../controllers/subscribtion');
+const authentication = require('../middlewares/authentication');
+
+const subscribtionRouter = express.Router();
+
+subscribtionRouter.post('/gym' , authentication , addGymUser );
+subscribtionRouter.post('/rest' , authentication , addRestaruntUser);
+subscribtionRouter.post('/trainer', authentication , addTrainerUser);
+
+module.exports = subscribtionRouter;
+
+
+
+
