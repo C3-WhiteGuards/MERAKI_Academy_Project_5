488ff1c7406377183ee170427bfa0a40ec87a770
G11_Kulthoom <kulthumradi94@gmail.com>
2021-10-11 22:54:46 +0300

    creating the resturent component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-
+import { Resturants } from './components/resturants/resturant';
 const App = () => {
-	return <div className="App">App component</div>;
+	return <div className="App">App component
+	<Resturants/>
+	
+	
+	
+	</div>;
+
 };
 
 export default App;
