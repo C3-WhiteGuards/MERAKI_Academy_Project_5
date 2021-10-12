a1af8a5dc196109fee6968c6a1d30c6adcd5c9f7
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-10-12 21:37:22 +0300

    gym dashbord created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import { Gym } from './components/Gym/Gym';
+import {Gym} from "./components/Gym/Gym";
 const App = () => {
 	return <div className="App">App component
 	<Gym/>
--- frontend/src/components/Gym/Gym.js
+++ frontend/src/components/Gym/Gym.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import "./Gym.css";
-
-// get all gymss function
 export const Gym = () => {
   const [gyms, setAllGym] = useState([]);
 
@@ -32,11 +30,13 @@ export const Gym = () => {
                   <h5>{element.phoneNumber}</h5>
                   <h5>{element.priceMonthly}</h5>
                   <h5>{element.description}</h5>
-                  <h5>
-                    <Link target="_blank" to={element.location}>
-                      {" "}
-                      see gym location
-                    </Link>
+                  <h5
+                    onClick={() => {
+                      window.open(element.location, "_blank");
+                    }}
+                  >
+                    {" "}
+                    LOCATION
                   </h5>
                   <button>subscribe</button>
                 </div>
@@ -48,68 +48,147 @@ export const Gym = () => {
   );
 };
 
-export const AddNewGym = () => {
-  const [name, setName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [image, setImage] = useState("");
-  const [priceMonthly, setPriceMonthly] = useState("");
-  const [description, setDescription] = useState("");
+// export const AddNewGym = () => {
+//   const [name, setName] = useState("");
+//   const [phoneNumber, setPhoneNumber] = useState("");
+//   const [image, setImage] = useState("");
+//   const [priceMonthly, setPriceMonthly] = useState("");
+//   const [description, setDescription] = useState("");
 
-  const addGym = () => {
-    axios
-      .post("http://localhost:5000/gym", {
-        name,
-        phoneNumber,
-        image,
-        priceMonthly,
-        description,
-      })
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.log(error.response);
-      });
-  };
+//   const addGym = () => {
+//     axios
+//       .post("http://localhost:5000/gym", {
+//         name,
+//         phoneNumber,
+//         image,
+//         priceMonthly,
+//         description,
+//       })
+//       .then((result) => {
+//         console.log(result);
+//       })
+//       .catch((error) => {
+//         console.log(error.response);
+//       });
+//   };
+//  return (
+//     <div>
+//       <input
+//         placeholder="name"
+//         type="text"
+//         onChange={(e) => {
+//           setName(e.target.value);
+//         }}
+//       ></input>
+//       <input
+//         placeholder="phoneNumber"
+//         type="number"
+//         onChange={(e) => {
+//           setPhoneNumber(e.target.value);
+//         }}
+//       ></input>
+//       <input
+//         placeholder="image Link"
+//         type="text"
+//         onChange={(e) => {
+//           setImage(e.target.value);
+//         }}
+//       ></input>
+//       <input
+//         placeholder="priceMonthly"
+//         type="text"
+//         onChange={(e) => {
+//           setPriceMonthly(e.target.value);
+//         }}
+//       ></input>
+//       <input
+//         placeholder="description"
+//         type="text"
+//         onChange={(e) => {
+//           setDescription(e.target.value);
+//         }}
+//       ></input>
+//       <button
+//         onClick={() => {
+//           addGym();
+//         }}
+//       >
+//         {" "}
+//         Add Gym
+//       </button>
 
-  return (
-    <div>
-      <input
-        placeholder="name"
-        type="text"
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
-      ></input>
-      <input
-        placeholder="phoneNumber"
-        type="number"
-        onChange={(e) => {
-          setPhoneNumber(e.target.value);
-        }}
-      ></input>
-      <input
-        placeholder="image Link"
-        type="text"
-        onChange={(e) => {
-          setImage(e.target.value);
-        }}
-      ></input>
-      <input
-        placeholder="priceMonthly"
-        type="text"
-        onChange={(e) => {
-          setPriceMonthly(e.target.value);
-        }}
-      ></input>
-      <input
-        placeholder="description"
-        type="text"
-        onChange={(e) => {
-          setDescription(e.target.value);
-        }}
-      ></input>
-      <button onClick={() => {addGym()}}> Add Gym</button>
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
+
+///////////////////////////////////////////////////////////////
+// export const UpdateGym = () => {
+//   const [name, setName] = useState("");
+//   const [phoneNumber, setPhoneNumber] = useState("");
+//   const [image, setImage] = useState("");
+//   const [priceMonthly, setPriceMonthly] = useState("");
+//   const [description, setDescription] = useState("");
+
+//   const updateGymById = (id) => {
+//     axios
+//       .put(`http://localhost:5000/gym/${id}`, {
+//         name,
+//         phoneNumber,
+//         image,
+//         priceMonthly,
+//         description,
+//       })
+//       .then((result) => {
+//         console.log(result);
+//         Gym();
+//       });
+//   };
+
+//   return (
+//     <div>
+//       <input
+//         placeholder="name"
+//         type="text"
+//         onChange={(e) => {
+//           setName(e.target.value);
+//         }}
+//       ></input>
+//       <input
+//         placeholder="phoneNumber"
+//         type="number"
+//         onChange={(e) => {
+//           setPhoneNumber(e.target.value);
+//         }}
+//       ></input>
+//       <input
+//         placeholder="image Link"
+//         type="text"
+//         onChange={(e) => {
+//           setImage(e.target.value);
+//         }}
+//       ></input>
+//       <input
+//         placeholder="priceMonthly"
+//         type="text"
+//         onChange={(e) => {
+//           setPriceMonthly(e.target.value);
+//         }}
+//       ></input>
+//       <input
+//         placeholder="description"
+//         type="text"
+//         onChange={(e) => {
+//           setDescription(e.target.value);
+//         }}
+//       ></input>
+//       <button
+//         onClick={() => {
+//           updateGymById();
+//         }}
+//       >
+//         {" "}
+//         Update Gym
+//       </button>
+//     </div>
+//   );
+// };
