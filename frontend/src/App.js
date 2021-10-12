b68877452dd017af8a2a4d85d563cbcc80c457ea
AbdullahNaser94 <abdulahnaser94@gmail.com>
2021-10-12 20:38:09 +0300

    add function attTrainers and editelement img in function getAllTrainers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-
+import { AddTrainer } from './components/trainer/trainer';
 import { Gym } from './components/Gym/Gym';
 import { AddProducts } from './components/products/product';
 const App = () => {
 	return <div className="App">App component
-	<AddProducts/>
+	<AddTrainer/>
+	
 	</div>;
 
 };
