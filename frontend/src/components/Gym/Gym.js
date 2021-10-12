3a3d3895a23713dc0c41022807c27565fdb70b44
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-10-12 21:57:56 +0300

    modify the gym component

--- frontend/src/components/Gym/Gym.js
+++ frontend/src/components/Gym/Gym.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import "./Gym.css";
+
 export const Gym = () => {
   const [gyms, setAllGym] = useState([]);
-
   useEffect(() => {
     axios
       .get("http://localhost:5000/gym")
@@ -16,25 +15,19 @@ export const Gym = () => {
         console.log(error.response);
       });
   }, []);
-
   return (
     <div className="gym">
       <div className="child">
-
         {gyms &&
           gyms.map((element, index) => {
             return (
               <div key={index}>
                 <img src={element.image} />
-
-
-
                 <div className="childtwo">
                   <h5>{element.name}</h5>
                   <h5>{element.phoneNumber}</h5>
                   <h5>{element.priceMonthly}</h5>
                   <h5>{element.description}</h5>
-
                   <h5
                     onClick={() => {
                       window.open(element.location, "_blank");
@@ -45,16 +38,11 @@ export const Gym = () => {
                   </h5>
                   <button>subscribe</button>
                 </div>
-
-               </div>
+              </div>
             );
           })}
       </div>
     </div>
   );
 };
-
-                  
- </div>
-)
 
