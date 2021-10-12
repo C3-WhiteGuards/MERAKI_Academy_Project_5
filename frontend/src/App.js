7fd0c5b23f083002aca3130d4441f4d6bc811edf
Islamshanableh <islam.alshanableh@gmail.com>
2021-10-12 22:23:03 +0300

    routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import { Gym } from './components/Gym/Gym';
-import { AddProducts } from './components/products/product';
-import { Login } from './components/auth/login';
+import React from "react";
+import { Route } from "react-router-dom";
+import { Gym } from "./components/Gym/Gym";
+import { AddProducts } from "./components/products/product";
+import { Login } from "./components/auth/login";
 
 const App = () => {
-	return (<div>
-    <Switch>
-    <Route path="/login" component={Login}/> 
-    
-   
-    
-  
-    
-    
-    
-    </Switch>
-    
-   
-      </div>)
+  return (
+    <div>
+      <Switch>
+        {/* islam */}
+        <Route path="/login" component={Login} />
 
+
+
+
+        {/* rashed */}
+
+
+
+
+
+        {/* roqia */}
+
+
+
+
+
+        {/* koulthom */}
+
+
+
+
+        {/* abdalllah */}
+
+
+
+        
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
