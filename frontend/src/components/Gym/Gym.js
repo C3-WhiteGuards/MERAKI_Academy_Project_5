2ef57c48714e7e05b02fc306f0db80cc6510ab49
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-10-12 14:00:39 +0300

    add link gym location

--- frontend/src/components/Gym/Gym.js
+++ frontend/src/components/Gym/Gym.js
@@ -1,41 +1,45 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import {Link} from "react-router-dom"
 
 export const Gym = () => {
   const [gyms, setAllGym] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/gym").then((res) => {
-      console.log(res.data);
-      setAllGym([...res.data.result]);
-    }).catch((error)=>{
+    axios
+      .get("http://localhost:5000/gym")
+      .then((res) => {
+        console.log(res.data);
+        setAllGym([...res.data.result]);
+      })
+      .catch((error) => {
         console.log(error.response);
-    })
+      });
   }, []);
   return (
-    <div className="gym" >
+    <div className="gym">
       <div>
-         {
-             gyms &&
-             gyms.map((element,index)=>{
-                 return (
-                     <div key={index}>
-                        
-    <h5>{element.name}</h5>
-    <h5>{element.phoneNumber}</h5>
-    <img src ={element.image}/>
-    {/* <h5><a target="_blank" href={element.location}> see gym location</a></h5> */}
-    <h5>{element.priceMonthly}</h5>
-    <h5>{element.description}</h5>
-    <button>subscribe</button>
+        {gyms &&
+          gyms.map((element, index) => {
+            return (
+              <div key={index}>
+                <img src={element.image} />
+                <h5>{element.name}</h5>
+                <h5>{element.phoneNumber}</h5>
+                <h5>{element.priceMonthly}</h5>
+                <h5>{element.description}</h5>
+                <h5>
+                  <Link target="_blank" to={element.location}>
+                    {" "}
+                    see gym location
+                  </Link>
+                </h5>
 
-                   </div>
-                 )
-             })
-         }
+                <button>subscribe</button>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
 };
-
-
