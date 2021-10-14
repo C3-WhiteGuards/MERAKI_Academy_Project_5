89b5d02044f50517402ab6364238a889c138c8c4
Islamshanableh <islam.alshanableh@gmail.com>
2021-10-15 00:12:27 +0300

    create loginGoogle

--- backend/routers/controllers/auth/login.js
+++ backend/routers/controllers/auth/login.js
@@ -2,20 +2,24 @@ const connection = require("../../../db/db");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const { OAuth2Client } = require("google-auth-library");
 
+const clientId = new OAuth2Client(
+  "707788443358-u05p46nssla3l8tmn58tpo9r5sommgks.apps.googleusercontent.com"
+);
 const login = (req, res) => {
   const password = req.body.password;
   const email = req.body.email.toLowerCase();
   const query = `SELECT * FROM users WHERE email ='${email}'`;
   connection.query(query, async (err, result) => {
     if (err) {
-      res.status(500).json({
+      return res.status(500).json({
         success: false,
         message: `Server Error`,
         error: err,
       });
     }
-    if (result.length === 0) {
+    if (!result.length) {
       return res.status(404).json({
         success: false,
         message: `The email doesn't exist`,
@@ -31,7 +35,6 @@ const login = (req, res) => {
       }
       const payload = {
         userId: result[0].id,
-        country: result[0].country,
         role: result[0].role,
       };
 
@@ -51,6 +54,78 @@ const login = (req, res) => {
   });
 };
 
+const loginGoogle = async (req, res) => {
+  const tokenId = req.body.tokenId;
+  client
+    .verifyIdToken({
+      idToken: tokenId,
+      audience:
+        "707788443358-u05p46nssla3l8tmn58tpo9r5sommgks.apps.googleusercontent.com",
+    })
+    .then((response) => {
+      const { email_verified, given_name, family_name, email } =
+        response.payload;
+      if (email_verified) {
+        const query = `SELECT * FROM users WHERE users.email=?`;
+        data = [email];
+        connection.query(query, data, async (err, result) => {
+          if (result.length) {
+            try {
+              const valid = await bcrypt.compare(
+                email + "123",
+                result[0].password
+              );
+
+              if (!valid) {
+                return res.status(403).json({
+                  success: false,
+                  message: "The password you’ve entered is incorrect",
+                });
+              }
+              const payload = {
+                userId: result[0].id,
+                role: result[0].role,
+              };
+
+              const options = {
+                expiresIn: "7d",
+              };
+              const token = jwt.sign(payload, process.env.SECRET, options);
+              return res.status(200).json({
+                success: true,
+                message: "Email and Password are correct",
+                token: token,
+              });
+            } catch (error) {
+              throw new Error(error.message);
+            }
+          } else {
+            let password = email + "123";
+            const bcryptPassword = await bcrypt.hash(password, 10);
+            const query = `INSERT INTO users (firstName,lastName,email,password) VALUES(?,?,?,?)`;
+            data = [given_name, family_name, email, bcryptPassword];
+            connection.query(query, data, (err, result) => {
+              if (result) {
+                res.status(201).json({
+                  success: true,
+                  message: `Create user successful`,
+                  result: result,
+                });
+              } else {
+                res.status(500).json({
+                  success: false,
+                  message: `Server Error`,
+                  err: err,
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+};
+
 module.exports = {
   login,
+  loginGoogle,
 };
