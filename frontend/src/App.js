10d14a8b84073e526d57140d9397a8527608d418
AbdullahNaser94 <abdulahnaser94@gmail.com>
2021-10-15 15:22:39 +0300

    add data trainer in file trainer.js and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,68 +1,50 @@
 import React from "react";
-import { Route,Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { Gym } from "./components/Gym/Gym";
 import { AddProducts } from "./components/products/product";
 import { Login } from "./components/auth/login";
 
-
 import { GymsView } from "./components/Sections/sections";
-import { AddTrainer,  Trainer } from "./components/trainer/trainer";
+import { AddTrainer, Trainer } from "./components/trainer/trainer";
 import { Resturants } from "./components/resturants/resturant";
 
-
-
 import Register from "./components/auth/signUp";
-import Navigation from "./components/navigation/index"
-import {Footer}  from "./components/Footer/index"
+import Navigation from "./components/navigation/index";
+import { Footer } from "./components/footer/index";
 import { Logout } from "./components/logout/Logout";
 
 import { OneTrainer } from "./components/trainer/OneTrainer";
 
-import About from "./components/Footer/About";
-import Policy from "./components/Footer/Policy";
+import About from "./components/footer/About";
+import Policy from "./components/footer/Policy";
 
 const App = () => {
   return (
     <div>
-
-
-<Navigation/> 
-
+      <Navigation />
 
       <Switch>
         {/* islam */}
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-        <Route path = '/logout' component = {Logout} />
-
-
-
+        <Route path="/logout" component={Logout} />
 
         {/* rashed */}
-        <Route path='/gymsview' component={GymsView} />
-		<Route path='/ALLGyms' component={Gym}/>
-		<Route path='/AllTrainers' component={Trainer}/>
-		<Route path='/AllRestaurnats' component={Resturants}/>
-		<Route exact path='/trainer/:id' component={OneTrainer}/>
+        <Route path="/gymsview" component={GymsView} />
+        <Route path="/ALLGyms" component={Gym} />
+        <Route path="/AllTrainers" component={Trainer} />
+        <Route path="/AllRestaurnats" component={Resturants} />
+        <Route exact path="/trainer/:id" component={OneTrainer} />
 
         {/* roqia */}
-<Route  path = '/boutUs' component = {About} />
-<Route  path = '/ourPolicy' component = {Policy} />
-
-
+        <Route path="/boutUs" component={About} />
+        <Route path="/ourPolicy" component={Policy} />
 
         {/* koulthom */}
 
-
-
-
         {/* abdalllah */}
-
-
-
-        
       </Switch>
-<Footer/>
+      <Footer />
     </div>
   );
 };
