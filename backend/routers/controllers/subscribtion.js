de1e13b22642109422c213c37c709364b96716e3
RuqiaHammouri <roqia.hammouri112@gmail.com>
2021-10-16 00:03:47 +0300

    subscribtion for gym function created

--- backend/routers/controllers/subscribtion.js
+++ backend/routers/controllers/subscribtion.js
@@ -6,7 +6,7 @@ const addGymUser = (req, res) => {
   const date = `${d.getFullYear()}-${d.getMonth() + 2}-${d.getDate()}`;
   const query = `INSERT INTO subscriptionsGym (userId , gymId , date_to) VALUES (?,?,?)`;
   const data = [userId, gymId, date];
-  connection.query(query, data, (err, result) => {
+  connection.query(query, data, (err, result ) => {
     if (err)
       return res
         .status(404)
--- frontend/src/components/Gym/Gym.js
+++ frontend/src/components/Gym/Gym.js
@@ -3,49 +3,80 @@ import axios from "axios";
 import "./Gym.css";
 
 export const Gym = () => {
-  const [gyms, setAllGym] = useState([]);
+  const [allgyms, setAllGyms] = useState([]);
+  const userId = localStorage.getItem("userId");
+  let gymId = "";
+  const token = localStorage.getItem("token");
+
   useEffect(() => {
     axios
       .get("http://localhost:5000/gym")
       .then((res) => {
         console.log(res.data);
-        setAllGym([...res.data.result]);
+        setAllGyms([...res.data.result]);
+        gymId = res.data.result;
+        console.log(allgyms);
       })
       .catch((error) => {
         console.log(error.response);
       });
   }, []);
-  return (
-    
-      <div >
-        {gyms &&
-          gyms.map((element, index) => {
-            return (
-              <div key={index} className="OneGym"> 
 
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeOLTtl_ZhcWlmWMExJ3t4P5cBnElgi9VbGA&usqp=CAU" className="imgGym"/>
-                <div className="childtwo">
-                  <h5 className="NameOfTheGym"> Golden Gym</h5>
-                  <h5> 📞 0777522486</h5>
-                  <h5 >  Monthly Subscribtion 350 JD</h5>
-                  <h5 className="desc">Are you up to the Pink Power challenge with the Ladies Fitness Team?
-Ready, Set, Go Pink on Saturday at 6:00 pm at the Ladies Gym – Gold’s Gym Abdoun for the chance to win Free Memberships and a Gold’s Pink Gym Bag.
-</h5>
-                  <h5
-                    onClick={() => {
-                      window.open(element.location, "_blank");
-                    }}
-                  >
-                    {" "}
-                    📍 LOCATION 
-                  </h5>
-                  <button className="subscribeBtn">Subscribe Now</button>
-                </div>
+  const addSubsGym = async (gymId) => {
+    await axios
+      .post(
+        `http://localhost:5000/subscribtion/gym`,
+        { gymId },
+        { headers: { Authorization: `Bearer ${token}` } }
+      )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  };
+
+  return (
+    <div>
+      {allgyms &&
+        allgyms.map((element, index) => {
+          return (
+            <div key={index} className="OneGym">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeOLTtl_ZhcWlmWMExJ3t4P5cBnElgi9VbGA&usqp=CAU"
+                className="imgGym"
+              />
+              <div className="childtwo">
+                <h5 className="NameOfTheGym"> Golden Gym</h5>
+                <h5> 📞 0777522486</h5>
+                <h5> Monthly Subscribtion 350 JD</h5>
+                <h5 className="desc">
+                  Are you up to the Pink Power challenge with the Ladies Fitness
+                  Team? Ready, Set, Go Pink on Saturday at 6:00 pm at the Ladies
+                  Gym – Gold’s Gym Abdoun for the chance to win Free Memberships
+                  and a Gold’s Pink Gym Bag.
+                </h5>
+                <h5
+                  onClick={() => {
+                    window.open(element.location, "_blank");
+                  }}
+                >
+                  {" "}
+                  📍 LOCATION
+                </h5>
+                <button
+                  className="subscribeBtn"
+                  onClick={() => {
+                    addSubsGym(element.id);
+                  }}
+                >
+                  Subscribe Now
+                </button>
               </div>
-            );
-          })}
-      </div>
-    
+            </div>
+          );
+        })}
+    </div>
   );
 };
-
