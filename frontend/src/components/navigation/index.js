b56b5e8f10bf209513de16d5f3ec82f86886afd9
Islamshanableh <islam.alshanableh@gmail.com>
2021-10-16 13:34:19 +0300

    fix functios

--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -1,67 +1,95 @@
 import { Login } from "../auth/login";
-import React from "react";
+import React ,{useEffect} from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import "./navigation.css"
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 
 const Navigation = () => {
-
-  const state = useSelector((state) => {
+  const state =useSelector((state) => {
     return {
-      token : state.token.token,
+      token: state.token.token,
     };
   });
 
-  return (
-    <nav className="fixed">
-         <div className="left">
-        <img src="/image.png"/>
-        </div>
-      {!state.token ? (
-<ul className="nav-list">
-          <div className="words">
-        <li className="nav-item">
-            <Link className="Link" to="/login">
-              login
-            </Link>
-            </li>
-          <li className="nav-item">
-          <Link className="Link" to="/signup">
-          signup
-            </Link> 
-          </li>
-          </div>
-</ul>
-      
-      ) : (
-        
-<ul className="nav-list">
-   <div className="words">
 
-           <li className="nav-item">
-            <Link className="Link" to="/logout">
-              logOut
-            </Link>
-            </li>
-          <li className="nav-item">
-            <Link className="Link" to="/home">
-              Home
-            </Link>
-          </li>
-        <li className="nav-item">
-            <Link className="Link" to="/profile">
-              profile
-            </Link>
-          </li> 
-    <li className="nav-item">
-            Profile 
-          </li>
-         </div>
-</ul>
+  return (
+   <>
+      {!state.token ? (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+        <Container>
+          <Navbar.Brand >
+            <Image className="logo" src="/image.png" width="45%" height="45%" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features"></Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="/home">Home</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
+              <Nav.Link eventKey={2} href="/register">
+                Register
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+        </Navbar>
+      ) : (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+        <Container>
+          <Navbar.Brand>
+            <Image className="logo" src="/image.png" width="45%" height="45%" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features"></Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="/logout">Logout</Nav.Link>
+              <Nav.Link href="/profile">Profile</Nav.Link>
+              <Nav.Link href="/home">Home</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+        </Navbar>
       )}
-    </nav>
+    </>
   );
 };
-
 
 export default Navigation;
--- frontend/src/redux/reducer/cart/index.js
+++ frontend/src/redux/reducer/cart/index.js
@@ -1,5 +1,5 @@
 const intialToken = {
-  items: JSON.parse(localStorage.getItem("savedData")),
+  items: JSON.parse(localStorage.getItem("savedData")) || [],
   total: 0,
 };
 
@@ -15,9 +15,7 @@ const cart = (state = intialToken, { type, payload }) => {
       let itemToRemove = state.items.find((item) => payload === item.id);
       let new_items = state.items.filter((item) => payload !== item.id);
       localStorage.setItem("savedData", JSON.stringify([...new_items]));
-      //calculating the total
       let newTotal = state.total - itemToRemove.price;
-      console.log(itemToRemove);
       return {
         items: [...new_items],
         total: newTotal,
