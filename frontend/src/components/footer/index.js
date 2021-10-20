d1a79debc6d06e8d13159883c2fc7cee21931903
AbdullahNaser94 <abdulahnaser94@gmail.com>
2021-10-20 23:27:52 +0300

    edit css footer

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,29 +1,25 @@
-import React from 'react';
-import { FaInstagram ,FaFacebook , FaTwitter  } from "react-icons/fa";
+import React from "react";
+import { FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import "./footer.css"
-import "./About"
-
-
-
+import "./footer.css";
+import "./About";
 
 export const Footer = () => {
-	
-	return <div className="footerMain">
-		<div className="copyRight"> 
-			<h5 > copyright © 2021 </h5>
-		</div>
-		<div className="iconDevMain">
-		<FaInstagram icon={FaInstagram} className="facebook" />
-		<FaFacebook icon={FaFacebook}   className="facebook"/>
-		<FaTwitter icon={FaTwitter}  className="facebook" />
-		</div>
-		
- <div className="Aboutfooter">
- <Link to="/boutUs"> About Us      </Link>
- <Link to="/ourPolicy"> Our Policy</Link>
- </div>
-	</div>;
+  return (
+    <div className="footerMain">
+      <div className="copyRight">
+        <h5> copyright © 2021 </h5>
+      </div>
+      <div className="iconDevMain">
+        <FaInstagram icon={FaInstagram} className="facebook" />
+        <FaFacebook icon={FaFacebook} className="facebook" />
+        <FaTwitter icon={FaTwitter} className="facebook" />
+      </div>
+
+      <div className="Aboutfooter">
+        <Link to="/boutUs"> About Us </Link>
+        <Link to="/ourPolicy"> Our Policy</Link>
+      </div>
+    </div>
+  );
 };
-
-
